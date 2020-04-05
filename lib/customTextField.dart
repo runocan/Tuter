c3011700304bdbd2e14d0ad8820d9ec627154a6b
Onur <runocan@outlook.com>
2020-04-05 18:52:27 -0400

    signup page

--- lib/customTextField.dart
+++ lib/customTextField.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class CustomTextField extends StatelessWidget {
   CustomTextField(
       {this.hint, this.password = false, this.validator, this.onSaved});
@@ -8,7 +9,7 @@ class CustomTextField extends StatelessWidget {
   final bool password;
   final FormFieldValidator<String> validator;
   final FormFieldSetter<String> onSaved;
-
+  
   @override
   Widget build(BuildContext context) {
     return Container(
